540238fefbe8375c7d78075a0009c695623d7511
Mario Marin <mariomarinmm18@gmail.com>
2024-08-25 12:15:30 -0500

    Creación Place Order Page

---
+++ cypress/e2e/pages/place-order/place-order.data.js
@@ -0,0 +1,12 @@
+export class PlaceOrderData{
+    static get testData(){
+        return{
+            name: 'Mario Marín',
+            country: 'Colombia',
+            city: 'Cartago Valle',
+            creditCardNumber: '5555555555554444',
+            month: 12,
+            year: 2028
+        };
+    }
+}
