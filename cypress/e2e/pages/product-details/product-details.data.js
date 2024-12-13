3fc2400abbbb7b6b99c68401d7d3944886b1592b
Mario Marin <mariomarinmm18@gmail.com>
2024-12-12 19:52:22 -0500

    Implementation of Product details data and catalog and purchases test cases

---
+++ cypress/e2e/pages/product-details/product-details.data.js
@@ -0,0 +1,31 @@
+export class ProductDetailsData{
+    static get Phones(){
+        return{
+            phone1: "Samsung galaxy s6",
+            phone2: "Nokia lumia 1520",
+            phone3: "Nexus 6",
+            phone4: "Samsung galaxy s7",
+            phone5: "Iphone 6 32gb",
+            phone6: "Sony xperia z5",
+            phone7: "HTC One M9"
+        }
+    }
+
+    static get Laptops(){
+        return{
+            laptop1: "Sony vaio i5",
+            laptop2: "Sony vaio i7",
+            laptop3: "MacBook air",
+            laptop4: "Dell i7 8gb",
+            laptop5: "2017 Dell 15.6 Inch",
+            laptop6: "MacBook Pro"
+        }
+    }
+
+    static get Monitors(){
+        return{
+            monitor1: "Apple monitor 24",
+            monitor2: "ASUS Full HD"
+        }
+    }
+}
--- cypress/e2e/tests/catolog-and-purchases.cy.js
+++ cypress/e2e/tests/catolog-and-purchases.cy.js
@@ -2,6 +2,7 @@ import { CommonMethods } from "../pages/common/common.methods";
 import { HomeMethods } from "../pages/home/home.methods";
 import { LoginData } from "../pages/login/login.data";
 import { LoginMethods } from "../pages/login/login.methods";
+import { ProductDetailsData } from "../pages/product-details/product-details.data";
 import { Logger } from "../util/logger";
 import { Suites } from "../util/suites";
 
@@ -25,8 +26,33 @@ describe(Suites.testSuites.catalogAndPurchases, ()=>{
         Logger.step('Clic on a product category')
         HomeMethods.clickOnMonitorsOption();
         Logger.verification('Check that shows the corresponding products list to the selected category')
-        HomeMethods.verifyProductDisplayed('Apple monitor 24')
-        HomeMethods.verifyProductDisplayed('ASUS Full HD')
+        HomeMethods.verifyProductDisplayed(ProductDetailsData.Monitors.monitor1)
+        HomeMethods.verifyProductDisplayed(ProductDetailsData.Monitors.monitor2);
+    })
+
+    it('Add product to the shopping car', ()=>{
+
+        Logger.stepNumber(1)
+        Logger.step('Log in with a registered user')
+        Logger.subStep('Navigate to Demoblaze')
+        CommonMethods.navigateToDemoBlaze()
+        Logger.subStep('Clic on Log In Option')
+        CommonMethods.clickOnLogInOption()
+        Logger.subStep('Insert a valid username and password')
+        LoginMethods.login(LoginData.validCredentials.username, LoginData.validCredentials.password);
+
+        Logger.stepNumber(2)
+        Logger.step('Navigate to home page')
+        CommonMethods.clickOnHomeOption();
+
+        Logger.stepNumber(3)
+        Logger.step('Clic on a product category')
+        HomeMethods.clickOnMonitorsOption();
+        
+        Logger.stepNumber(4)
+        Logger.step('Clic on a specific product')
+        HomeMethods.clickOnProductLink(ProductDetailsData.Monitors.monitor1)
+        Logger.verify
     })
 })
 
